38785497572c7ac40ab4b06b345f04ab9db4ffc9
Ollie Badrick <badricoliv@ecs.vuw.ac.nz>
2021-06-29 20:51:33 +1200

    grid is drawable
    fixed 10x10 grid, can change color of each square, can choose whether to have visible grid lines

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,27 +1,47 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Grid, ServerStyleSheets, StylesProvider } from '@material-ui/core';
+import { AlertManager, useAlert } from 'react-alert'
 
 
 const Home = () => {
+    var text : Number = 1
+    const [origColor,setNewColor] = useState('#000000')
     const [count, setCount] = useState(0); //This is a hook, allowing for dynamic changes to the display
-    const gridColors = ['FFF000','FFF000','FFF000','FFF000','FFF000',
-                        'FFF000','FFF000','FFF000','FFF000','FFF000',
-                        'FFF000','FFF000','FFF000','FFF000','FFF000',
-                        'FFF000','FFF000','FFF000','FFF000','FFF000',
-                        'FFF000','FFF000','FFF000','FFF000','FFF000'
+    const gridColors = ['#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
+    '#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF',
                     ]
+    
     return (
         <View style={[styles.container,{backgroundColor:'#FFFFFF'}]}>
             <View style={styles.grid}>
                 {gridColors.map((squareColor) => {
+                    const [color,setColor] = useState(squareColor)
                     return (
-                        <View style={[styles.square,{backgroundColor:(squareColor)}]}></View>
+                        <TouchableOpacity onPress={()=>{setColor(origColor)}}>
+                            <View style={[styles.square,{backgroundColor:(color)},{borderWidth:count}]}></View>
+                        </TouchableOpacity>
                     )
                 })}
             </View>
-        </View>
+            <TouchableOpacity onPress={()=>{setNewColor('#FFFFFF')}}>White</TouchableOpacity>
+            <TouchableOpacity onPress={()=>{setNewColor('#000000')}}>Black</TouchableOpacity>
+            <TouchableOpacity onPress={()=>{setNewColor('#ff0000')}}>Red</TouchableOpacity>
+            <TouchableOpacity onPress={()=>{setNewColor('#999999')}}>Grey</TouchableOpacity>
 
+
+            <TouchableOpacity onPress={()=>{setCount(1)}}>Grid</TouchableOpacity>
+            <TouchableOpacity onPress={()=>{setCount(0)}}>No Grid</TouchableOpacity>
+
+        </View>
     );
     
 }
@@ -33,19 +53,18 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     grid: {
-        width: 300,
-        height:250,
+        width: 502,
+        height:500,
       flex: 1,
       flexDirection: 'row',
       flexWrap: 'wrap',
-      padding: 2,
+      padding: 0,
+      borderWidth:1
     },
     square: {
         margin: 0,
         width: 50,
         height: 50,
-        borderWidth: 1,
-        borderColor: '#000000',
     }
   });
 
